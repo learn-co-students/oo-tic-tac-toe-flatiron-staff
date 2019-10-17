d34f901610197ccabc9253da36f729a477613977
Rupinder Dhillon <rupinder.dhillon@flatironschool.com>
2019-10-17 04:17:01 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,93 @@
+class TicTacToe
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ].freeze
+
+  def initialize
+    @board = Array.new(9, ' ')
+  end
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts '-----------'
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts '-----------'
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(input)
+    input.to_i - 1
+  end
+
+  def move(idx, marker = 'X')
+    @board[idx] = marker
+  end
+
+  def position_taken?(idx)
+    !(@board[idx].strip == '' || @board[idx].nil?)
+  end
+
+  def valid_move?(idx)
+    idx.between?(0, 8) && !position_taken?(idx)
+  end
+
+  def turn_count
+    @board.reduce(0) { |sum, space| space.include?('X') || space.include?('O') ? sum + 1 : sum }
+  end
+
+  def current_player
+    turn_count % 2 == 0 ? 'X' : 'O'
+  end
+
+  def turn
+    puts 'Choose a space 0 - 8:'
+    input = gets.strip
+    idx = input_to_index(input)
+
+    if valid_move?(idx)
+      move(idx, current_player)
+      display_board
+    else
+      turn
+    end
+  end
+
+  def full?
+    @board.all? { |space| space.include?('X') || space.include?('O') }
+  end
+
+  def won?
+    WIN_COMBINATIONS.find { |combo| combo.all? { |idx| @board[idx] == 'X' } || combo.all? { |idx| @board[idx] == 'O' } }
+  end
+
+  def draw?
+    !won? && full?
+  end
+
+  def over?
+    won? || draw?
+  end
+
+  def winner
+    combo = won?
+
+    return nil if combo.nil?
+
+    @board[combo[0]]
+  end
+
+  def play
+    until over?
+      turn
+    end
+
+    puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
+  end
+end
